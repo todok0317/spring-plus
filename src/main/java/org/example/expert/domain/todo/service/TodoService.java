d4f9254537a8a9729ab9729b9a34da3d327d8fff
todok0317 <todok0317@naver.com>
2025-04-30 12:54:45 +0900

    Lv1 - 1 : 코드 개선 퀴즈

--- src/main/java/org/example/expert/domain/todo/service/TodoService.java
+++ src/main/java/org/example/expert/domain/todo/service/TodoService.java
@@ -25,6 +25,8 @@ public class TodoService {
     private final TodoRepository todoRepository;
     private final WeatherClient weatherClient;
 
+
+    @Transactional
     public TodoSaveResponse saveTodo(AuthUser authUser, TodoSaveRequest todoSaveRequest) {
         User user = User.fromAuthUser(authUser);
 
